7b2d0285e789b7933a68b12f89befc1666007281
Kush <kushpatel124@gmail.com>
2022-11-24 19:31:02 -0500

    commit tree 87125f55b3e44164d24dd97bad4cc8be774bb399

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+const num1 = 6.2;
+const num2 = 10;
+let multiply = num1 * num2;
 
-// Write your code here
+let random = Math.floor(Math.random()*100);
+
+const num3 = 10;
+const num4 = 6;
+let mod = num3 % num4;
+
+let max = Math.max(2,3,20)
